22bb374667ca74c4e8b590cdd709754286cb1a3d
Prithvi Kannan <prithvi.kannan@gmail.com>
2019-12-07 13:42:24 -0800

    Added prereqs to makefile

--- assignment6/randmain.mk
+++ assignment6/randmain.mk
@@ -1,12 +1,12 @@
-randmain:
+randmain: randmain.c randcpuid.c 
 	$(CC) $(CFLAGS) -c randmain.c -o randmain.o
 	$(CC) $(CFLAGS) -c randcpuid.c -o randcpuid.o
 	$(CC) $(CFLAGS) -o randmain randmain.o randcpuid.o -ldl -Wl,-rpath=$(PWD) 
 
-randlibhw.so:
+randlibhw.so: randlibhw.c
 	$(CC) $(CFLAGS) -c -fPIC randlibhw.c -o randlibhw.o
 	$(CC) $(CFLAGS) -shared randlibhw.o -o randlibhw.so
 
-randlibsw.so:
+randlibsw.so: randlibsw.c
 	$(CC) $(CFLAGS) -c -fPIC randlibsw.c -o randlibsw.o
 	$(CC) $(CFLAGS) -shared randlibsw.o -o randlibsw.so
